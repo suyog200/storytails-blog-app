01e0d7eda42ffe6ca73286d2f2bdf383b2965623
suyog200 <suygchari@gmail.com>
2024-07-11 12:48:13 +0530

    basic setup and added footer and navbar component

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href='/'>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quasi, aliquid animi ab distinctio saepe modi illum nam voluptatum nobis ducimus explicabo dolor a, sapiente, temporibus dignissimos quidem at facere rem deserunt neque non sed autem similique! Iste, aut molestiae modi officia deserunt quo? Enim quos adipisci sequi tempore voluptatibus!</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia doloremque reiciendis ipsum, doloribus quas sint itaque cum totam quae eligendi, natus porro mollitia voluptatum officiis vero laudantium amet! Odit voluptatem sunt unde doloremque animi, labore esse dignissimos reiciendis libero at impedit a nulla est beatae quos exercitationem voluptatum aliquam? Maxime modi reiciendis porro labore fuga nesciunt maiores eum quae? Nihil harum et aliquam officiis quidem, nemo veniam, iure numquam, ipsum quos aut cum! Earum quisquam eius mollitia sunt recusandae nam aliquam alias, laboriosam culpa optio cumque molestiae quasi temporibus perspiciatis inventore repellendus velit, sit adipisci omnis doloremque officiis consequatur sapiente!
+      </Link>
+    </div>
+  );
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+export default function Navbar() {
+    return (
+        <div className={styles.container}>
+            footer
+        </div>
+    );
+}
