d2f9726d2bb85c07bddd4ac24d71746b0b8a09df
suyog200 <suygchari@gmail.com>
2024-07-15 11:28:31 +0530

    featured container added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
-export default function Navbar() {
+export default function Featured() {
     return (
         <div className={styles.container}>
-            featured
+            <h1 className={styles.title}>
+                <b>Hey, StoryTails here! </b>
+                Discover the world of stories and tales with us.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, omnis!</h1>
+                    <p className={styles.postDesc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ad excepturi nulla perferendis itaque nam quod cum est explicabo deserunt.</p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
         </div>
     );
 }
