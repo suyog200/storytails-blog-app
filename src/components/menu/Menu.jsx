0fae42a623ac1e092dc7fcb66dc52894bd38c49e
suyog200 <suygchari@gmail.com>
2024-07-16 14:55:56 +0530

    add menu post and pagination

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/menuCategories";
 import styles from "./menu.module.css";
 
-export default function Navbar() {
-    return (
-        <div className={styles.container}>
-            Menu
-        </div>
-    );
+
+export default function Menu() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false}/>
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editor's pick</h1>
+      <MenuPosts withImage={true}/>
+    </div>
+  );
 }
