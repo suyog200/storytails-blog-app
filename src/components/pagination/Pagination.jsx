29885e80a40058a5bd6fce89c52446d4e31aa051
suyog200 <suygchari@gmail.com>
2024-07-24 15:28:59 +0530

    integrated paggination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,11 +1,30 @@
+"use client";
+
 import React from "react";
 import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
 
-export default function Pagination() {
-    return (
-        <div className={styles.container}>
-            <button className={styles.button}>Previous</button>
-            <button className={styles.button}>Next</button>
-        </div>
-    );
-}
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        disabled={!hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        disabled={!hasNext}
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
