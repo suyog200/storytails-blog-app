5cad18aa09455e49257b3dac10dd083acecc1665
suyog200 <suygchari@gmail.com>
2024-07-26 15:06:57 +0530

    fetch & show user comments

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,10 +1,29 @@
+"use client";
 import React from "react";
 import styles from "./comments.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
-export default function Comments() {
-  const status = "authenticated";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+
+  return data;
+};
+
+export default function Comments({postSlug}) {
+  const status = useSession();
+  const {data,isLoading} = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher);
+
+  console.log(data);
 
   return (
     <div className={styles.container}>
@@ -18,58 +37,28 @@ export default function Comments() {
         <Link href="/login">Login to write comment</Link>
       )}
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              className={styles.image}
-              alt=""
-              width={50}
-              height={50}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Suyog Chari</span>
-              <span className={styles.date}>17.07.2024</span>
-            </div>
-          </div>
-            <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa?</p>
-        </div>
-      </div>
-      <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              className={styles.image}
-              alt=""
-              width={50}
-              height={50}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Suyog Chari</span>
-              <span className={styles.date}>17.07.2024</span>
-            </div>
-          </div>
-            <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa?</p>
-        </div>
-      </div>
-      <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              className={styles.image}
-              alt=""
-              width={50}
-              height={50}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Suyog Chari</span>
-              <span className={styles.date}>17.07.2024</span>
-            </div>
-          </div>
-            <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa?</p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
